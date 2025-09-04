65c3f07f87241242cfd1adbb879be03c3001de0b
Bk Ransford <bkransford0@gmail.com>
2025-09-04 11:12:16 +0000

    Add UserProfile and integrate into App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
@@ -8,6 +9,11 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile
+	name="Ransford"
+	age="25"
+	bio="Loves hiking, designing and games"
+      />
     </div>
   );
 }
